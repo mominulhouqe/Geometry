859f48c293652b8fd0dc6876004f62a6992b8f25
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-02-18 01:07:29 +0600

    paralleloGram added

---
+++ js/paralllelogram.js
@@ -0,0 +1,24 @@
+function calculateParallelGramArea() {
+    const paralleloGramHieghtInputField = document.getElementById('parallelo-width-input');
+    const paralleloGramHieghtInputFieldString = paralleloGramHieghtInputField.value;
+    const paralleloGramHieghtInputFieldValue = parseFloat(paralleloGramHieghtInputFieldString);
+  
+    const paralleloGramLengthField = document.getElementById('parallelo-length-input');
+    const paralleloGramLengthFieldString = paralleloGramLengthField.value;
+    const paralleloGramLengthFieldValue = parseFloat(paralleloGramLengthFieldString);
+  
+    const rectangelTotal = paralleloGramHieghtInputFieldValue * paralleloGramLengthFieldValue;
+
+  
+    const resultElement = document.getElementById('paralleloGram-area-result');
+    const resultElementString = resultElement.innerText;
+    let resultElementValue = parseFloat(resultElementString);
+
+    resultElement.innerText = rectangelTotal.toFixed(1);
+
+  }
+  
+ document.getElementById('paralleloGram-btn').addEventListener('click' , function(){
+    calculateParallelGramArea();
+ })
+  
